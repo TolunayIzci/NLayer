df64c8c73a34609b1a316a738c031a4673b298a8
Tolunay <tolunay_izci@hotmail.com>
2021-01-08 16:03:27 +0300

    First update

---
+++ UdemyNLayerProject.Core/Services/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyNLayerProject.Core.Services
+{
+    public interface ICategoryService
+    {
+    }
+}
---
+++ UdemyNLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Repositories;
+
+namespace UdemyNLayerProject.Core.UnitOfWorks
+{
+    interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository categoryies { set; }
+
+        Task CommitAsync();
+
+        void Commit();
+    }
+}
